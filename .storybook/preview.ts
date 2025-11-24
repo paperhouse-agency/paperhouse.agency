3f8effd30b7cee554d09f9e65f8d91828cc74247
Zubayer <zubayer@cloudly.io>
2025-11-24 21:14:46 +0600

    init(storybook)

---
+++ .storybook/preview.ts
@@ -0,0 +1,22 @@
+import type { Preview } from '@storybook/nextjs'
+import '../styles/css/index.css'
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+    backgrounds: {
+      default: 'dark',
+      values: [
+        { name: 'dark', value: '#000000' },
+        { name: 'light', value: '#ffffff' },
+      ],
+    },
+  },
+}
+
+export default preview
