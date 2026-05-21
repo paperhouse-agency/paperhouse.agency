26d561b35608aa0e4be44184d5eaef2d0bcc3b09
Zubayer <zubayer@cloudly.io>
2026-05-21 14:48:04 +0600

    chore: reconfigure middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
+export function proxy(request: NextRequest) {
   if (process.env.MAINTENANCE_MODE !== 'true') return NextResponse.next()
 
   const bypassSecret = process.env.MAINTENANCE_BYPASS_SECRET
